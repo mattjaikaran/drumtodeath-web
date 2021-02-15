760f2091f923414c94de10855a3430de63ab67ab
Matthew Jaikaran <mattjaikaran>
2021-02-15 17:56:27 -0500

    added return to api call

--- pages/api/exercises/index.js
+++ pages/api/exercises/index.js
@@ -20,5 +20,5 @@ function runMiddleware(req, res, fn) {
 
 export default async function handler(req, res) {
   await runMiddleware(req, res, cors)
-  res.status(200).json(exercises)
+  return res.status(200).json(exercises)
 }
